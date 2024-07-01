b18d085ca33e0132b8d035d48b69f10e4f90e7d7
Luong Cong Han <luongconghan.ute@gmail.com>
2024-07-01 19:40:54 +0700

    ff

--- UpdateMyApp/frmMain.Designer.cs
+++ UpdateMyApp/frmMain.Designer.cs
@@ -57,7 +57,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(59, 165);
+            this.button1.Location = new System.Drawing.Point(87, 164);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 23);
             this.button1.TabIndex = 2;
